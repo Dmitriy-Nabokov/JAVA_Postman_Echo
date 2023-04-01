860d46250d246691a5e9457a56fbf637d5c2ff5d
Neto test <loktionov.dv@mail.ru>
2023-04-01 20:15:37 +0300

    Initial

---
+++ src/test/java/ru/netology/rest/PostmanEcho.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldMakePOSTRequest() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Current date: 04.11.2022")
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("Current date: 04.11.2022"));
+
+    }
+}
+
